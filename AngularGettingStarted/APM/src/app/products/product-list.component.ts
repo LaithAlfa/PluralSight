bd226f13a94ab9ee44410f255f4a086e2819799d
laithalfa <laith.alfaloujeh@gmail.com>
2021-02-06 21:47:18 -0600

    add logic with directives

--- AngularGettingStarted/APM/src/app/products/product-list.component.ts
+++ AngularGettingStarted/APM/src/app/products/product-list.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
 })
 
 export class ProductListComponent {
-
+    pageTitle: string = 'Product List';
+    products: any[]= [
+      {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2019",
+        "description": "15 gallon capacity rolling garden",
+        "price": 32.99,
+        "startRating": 4.2,
+        "imageUrl": "assets/images/garden_cart.png",
+      },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "March 18, 2012",
+        "description": "CUrved Claw steel hammer",
+        "price": 8.9,
+        "startRating": 4.8,
+        "imageUrl": "assets/images/hammer.png",
+      }
+    ];
 }
