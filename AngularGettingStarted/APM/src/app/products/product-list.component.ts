2d889d167e1e8c85eaf602d072af7b7ad72ebb65
laithalfa <laith.alfaloujeh@gmail.com>
2021-02-06 22:00:19 -0600

    add logic with directives2

--- AngularGettingStarted/APM/src/app/products/product-list.component.ts
+++ AngularGettingStarted/APM/src/app/products/product-list.component.ts
@@ -15,7 +15,7 @@ export class ProductListComponent {
         "releaseDate": "March 18, 2019",
         "description": "15 gallon capacity rolling garden",
         "price": 32.99,
-        "startRating": 4.2,
+        "starRating": 4.2,
         "imageUrl": "assets/images/garden_cart.png",
       },
       {
@@ -25,7 +25,7 @@ export class ProductListComponent {
         "releaseDate": "March 18, 2012",
         "description": "CUrved Claw steel hammer",
         "price": 8.9,
-        "startRating": 4.8,
+        "starRating": 4.8,
         "imageUrl": "assets/images/hammer.png",
       }
     ];
